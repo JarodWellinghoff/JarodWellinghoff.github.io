ba0c4e285ff4037c6f5142baa4be931ba4e84caf
Skellinghoff <jarodwellinghoff@gmail.com>
2023-04-06 20:42:21 -0500

    react

---
+++ myscript.js
@@ -0,0 +1,8 @@
+function toggleSidebar() {
+	var sidebar = document.querySelector('.sidebar');
+	if (sidebar.style.left === '-200px') {
+		sidebar.style.left = '0';
+	} else {
+		sidebar.style.left = '-200px';
+	}
+}
