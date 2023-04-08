0e770a810a925219b6197616ddc4a513e1f86cf8
Skellinghoff <jarodwellinghoff@gmail.com>
2023-04-08 14:15:57 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 function App() {
 	return (
-		<div className="container mx-auto sm:bg-white md:bg-gray-500 lg:bg-black rounded-xl shadow border p-8 m-10">
-			<p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
-			<p className="text-gray-500 text-lg">React and Tailwind CSS in action</p>
+		<div
+			className="container m-10 mx-auto rounded-xl border p-8 shadow 
+        sm:border-black sm:bg-white
+        md:border-gray-500 md:bg-gray-500
+        lg:border-white lg:bg-black"
+		>
+			<p className="mb-5 text-3xl font-bold text-gray-700">Welcome!</p>
+			<p className="text-lg text-gray-500">React and Tailwind CSS in action</p>
 		</div>
 	);
 }
