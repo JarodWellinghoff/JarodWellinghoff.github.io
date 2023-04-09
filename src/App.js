7b28b2ab662db74810d68067e9d7f626efd416d9
Jarod Wellinghoff <jarodwellinghoff@gmail.com>
2023-04-08 22:41:16 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Resume from './components/Resume';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+
 function App() {
 	return (
-		<div
-			className="container m-10 mx-auto rounded-xl border p-8 shadow 
-        sm:border-black sm:bg-white
-        md:border-gray-500 md:bg-gray-500
-        lg:border-white lg:bg-black"
-		>
-			<p className="mb-5 text-3xl font-bold text-gray-700">Welcome!</p>
-			<p className="text-lg text-gray-500">React and Tailwind CSS in action</p>
-		</div>
+		<>
+			<Header />
+			<Resume />
+			<Projects />
+			<Footer />
+		</>
 	);
 }
 export default App;
